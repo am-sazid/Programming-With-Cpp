80efcc99fbf18025a3c0a3af996846db15c8c8c3
am-sazid <amsazid.cse@gmail.com>
2025-06-26 20:07:47 +0600

    Test

---
+++ Basic/variable.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main() {
+    // int x; // declaration
+    // x = 9; // initialization
+    // int x = 10; change variable value
+    int x , y ;
+    x = 10;
+    y = 10;
+    // int x = 5 , y = 6 ;
+    cout<<x*y; // Print Variable
+    
+}
