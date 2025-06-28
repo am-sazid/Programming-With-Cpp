83329ed0db1faaf2fdbb82c100bceda87db955b3
am-sazid <amsazid.cse@gmail.com>
2025-06-29 00:05:41 +0600

    user input

---
+++ Basic/problem_Day01.cpp
@@ -0,0 +1,37 @@
+// Problem 01
+// #include<iostream>
+
+// using namespace std;
+// int main() {
+//     float r = 3;
+//     float area = 3.1415 * r*r;
+//     cout<<"Area Is : ";
+//     cout<<area<<endl;
+// }
+
+
+// Problem 02
+// #include<iostream>
+
+// using namespace std;
+// int main() {
+//     float p = 1200 , r = 10 , t = 1 ;
+//     float si = (p*r*t) /100 ;
+//     cout<<"Total Interest : ";
+//     cout<<si;
+//     cout<<" Tk"<<endl;
+// }
+
+// Problem 03
+
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    float r = 15;
+    float v = (4 * 3.14 * r * r * r) / 3;
+    cout<<v<<endl;
+
+}
+
---
+++ Basic/user_input.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+
+using namespace std;
+int main(){
+    int x ;
+    cout<<"Enter Your Number : ";
+    cin>>x;
+    cout<<"Square of x is : ";
+    cout<<x*x<<endl;
+}
