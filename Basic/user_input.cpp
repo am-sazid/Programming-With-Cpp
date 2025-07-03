c2464b0959b8a5d86caf0ca5c90dd0353252c8f3
am-sazid <amsazid.cse@gmail.com>
2025-07-03 21:42:53 +0600

    User Input

--- Basic/user_input.cpp
+++ Basic/user_input.cpp
@@ -5,6 +5,11 @@ int main(){
     int x ;
     cout<<"Enter Your Number : ";
     cin>>x;
-    cout<<"Square of x is : ";
-    cout<<x*x<<endl;
+    int y ;
+    cout<<"Enter another Number : ",
+    cin>>y;
+    cout<<"Sum of x and y is : ";
+    cout<<x+y<<endl;
+
+
 }
