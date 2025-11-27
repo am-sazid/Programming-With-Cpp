6028825eab81aec8d33f64ada7bb697f28812782
am-sazid <amsazid.cse@gmail.com>
2025-11-28 01:41:19 +0600

    Strint

---
+++ Module 5/string.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+
+    string s = "Sazid";
+    s = "Name";
+
+    cout<< s << endl;
+    return 0;
+}
---
+++ Module 5/string_capacity.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+
+    string s = "Sazid";
+    s = "Name";
+
+    // cout<< s.size() << endl;
+    // cout<< s.max_size() << endl;
+    // cout<< s.capacity() << endl;
+    // cout<< s << endl;
+    // s.clear();
+    cout<< s<<endl;
+    // cout<< s.size()<<endl;
+    s.resize(10,'e');
+    cout<< s<< endl;
+    return 0;
+}
